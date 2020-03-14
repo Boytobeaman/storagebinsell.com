ca122d7f51e6f2382b64974509d5a89aeec32bcd
Your Name <you@example.com>
2020-03-14 12:11:16 +0800

    initial

---
+++ wp-content/themes/giga-store/footer.php
@@ -0,0 +1,51 @@
+<?php if ( is_active_sidebar( 'giga-store-footer-area' ) ) { ?>
+	<div class="footer-widgets"> 
+		<div class="container">		
+			<div id="content-footer-section" class="row clearfix">
+				<?php dynamic_sidebar( 'giga-store-footer-area' ) ?>
+			</div>
+		</div>
+	</div>	
+<?php } ?>
+<footer id="colophon" class="rsrc-footer" role="contentinfo">
+	<div class="container">  
+		<div class="row rsrc-author-credits">
+			<div class="text-center text-muted">
+				Copyright © 2019 | All Rights Reserved
+			</div>
+		</div>
+	</div>       
+</footer> 
+<p id="back-top">
+	<a href="#top"><span></span></a>
+</p>
+<!-- end main container -->
+</div>
+<nav id="menu" class="off-canvas-menu">
+	<?php
+	wp_nav_menu( array(
+		'theme_location' => 'main_menu',
+		'container'		 => false,
+	) );
+	?>
+</nav>
+<?php wp_footer(); ?>
+
+<div id="contactUs" class="modal fade" role="dialog">
+	<div class="modal-dialog">
+
+		<!-- Modal content-->
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+				<h4 class="modal-title">Request a Free Quote</h4>
+			</div>
+			<div class="modal-body">
+		<?php echo do_shortcode( '[contact-form-7 id="244" title="Contact form 1"]' ); ?>
+			</div>
+		</div>
+	</div>
+</div>
+
+</body>
+</html>
