b8646e46bf200739b822a12315e2530992323dcf
luxingchao <luxingchao@luxingchaodeMBP.lan>
2021-04-04 17:09:17 +0800

    update

--- wp-content/plugins/theme-customisations/custom/custom.js
+++ wp-content/plugins/theme-customisations/custom/custom.js
@@ -1,18 +1,21 @@
 ;jQuery(function($){
 	$(document).on("click",".product a.woocommerce-LoopProduct-link,.product-inquiry", function(e){
-		e.stopPropagation();
-		e.preventDefault();
 		
 		if($(e.target).hasClass("product-model") || $(e.target).closest(".product-model").length){
-			
+			e.stopPropagation();
+			e.preventDefault();
 		}else if($(e.target).hasClass("product-cat-inquiry") || $(e.target).closest(".product-cat-inquiry").length){
 			//加$(e.target).closest(".product-cat-inquiry").length 为了兼容 Gtranslate
-			
+			e.stopPropagation();
+			e.preventDefault();
+
 			var productModel = $(this).find(".product-model").attr("data-value");
 			$("#ProductModel").val(productModel);
 			$("#contactUs").modal();
 		}else if($(e.target).hasClass("product-inquiry") || $(e.target).closest(".product-inquiry").length){
-			
+			e.stopPropagation();
+			e.preventDefault();
+
 			$("#ProductModel").val($('.product-model .value').attr("data-value"));
 			$("#contactUs").modal();
 		}
