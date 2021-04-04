c7028ea975b4d1d96d3bed09474a26e0cf9f3b06
luxingchao <luxingchao@luxingchaodeMBP.lan>
2021-04-04 16:52:35 +0800

    prevent default jump of inquiry button

--- wp-content/plugins/theme-customisations/custom/custom.js
+++ wp-content/plugins/theme-customisations/custom/custom.js
@@ -1,15 +1,17 @@
 ;jQuery(function($){
 	$(document).on("click",".product a.woocommerce-LoopProduct-link,.product-inquiry", function(e){
+		e.preventDefault();
+		
 		if($(e.target).hasClass("product-model") || $(e.target).closest(".product-model").length){
-			e.preventDefault();
+			
 		}else if($(e.target).hasClass("product-cat-inquiry") || $(e.target).closest(".product-cat-inquiry").length){
 			//加$(e.target).closest(".product-cat-inquiry").length 为了兼容 Gtranslate
-			e.preventDefault();
-
+			
 			var productModel = $(this).find(".product-model").attr("data-value");
 			$("#ProductModel").val(productModel);
 			$("#contactUs").modal();
 		}else if($(e.target).hasClass("product-inquiry") || $(e.target).closest(".product-inquiry").length){
+			
 			$("#ProductModel").val($('.product-model .value').attr("data-value"));
 			$("#contactUs").modal();
 		}
